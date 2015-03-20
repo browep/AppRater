2758bfb43b9ffd3715302f7e942f3b2e16cfe3dd
Paul Brower <brower.paul@gmail.com>
2015-03-20 15:51:50 -0600

    include new copy text

--- AppRater/src/main/java/org/codechimp/apprater/AppRater.java
+++ AppRater/src/main/java/org/codechimp/apprater/AppRater.java
@@ -248,13 +248,13 @@ public class AppRater {
             builder = new AlertDialog.Builder(context);
         }
         ApplicationRatingInfo ratingInfo = ApplicationRatingInfo.createApplicationInfo(context);
-        builder.setTitle(String.format(context.getString(R.string.dialog_title), ratingInfo.getApplicationName()));
+        builder.setTitle(String.format(context.getString(R.string.app_rater_dialog_title), ratingInfo.getApplicationName()));
 
-        builder.setMessage(context.getString(R.string.rate_message));
+        builder.setMessage(context.getString(R.string.app_rater_rate_message));
 
         builder.setCancelable(isCancelable);
 
-        builder.setPositiveButton(context.getString(R.string.rate),
+        builder.setPositiveButton(context.getString(R.string.app_rater_rate_button),
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         rateNow(context);
@@ -266,7 +266,7 @@ public class AppRater {
                     }
                 });
 
-        builder.setNeutralButton(context.getString(R.string.later),
+        builder.setNeutralButton(context.getString(R.string.app_rater_later_button),
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         if (editor != null) {
